4f190ffd2dab267ed5a7ee86bf0c58a7bd9a6910
Annika <annika.knepper@gmx.de>
2022-10-28 13:39:40 +0200

    getDistance, equals

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -24,6 +24,32 @@ public class Coordinate {
     public double getZ() {
         return z;
     }
+
+    public double getDistance(Coordinate coor) {
+        if (coor == null)
+            return -1;
+        return Math.sqrt(Math.pow(x - coor.getX(), 2) +
+                Math.pow(y - coor.getY(), 2) +
+                Math.pow(z - coor.getZ(), 2));
+    }
+    public boolean isEqual(Coordinate coor) {
+        if (coor == null)
+            return false;
+        return x == coor.getX() && y == coor.getY() && z == coor.getZ();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (! (obj instanceof Coordinate))  //checks for null, too
+            return false;
+        return this.isEqual((Coordinate) obj);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
     public static Coordinate readFrom(ResultSet rset) throws SQLException {
         return new Coordinate(rset.getDouble("coordinate_x"),
                 rset.getDouble("coordinate_y"), rset.getDouble("coordinate_z"));
